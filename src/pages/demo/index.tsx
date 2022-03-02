f907b81ed983ad74e898c8d9860e79c9e9c13fbd
梁健辉 <ljk15916807596@qq.com>
2022-03-02 14:46:05 +0800

    fix: 新增试探性实现两个关联下拉框代码

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -10,28 +10,42 @@ const { TextArea } = Input;
 const { Option } = Select;
 
 const DrawerForm = (props: any) => {
-  const { cref, confirm } = props;
-
   const [form] = Form.useForm();
-
-  const [visible, setVisible] = useState<boolean>(false);
-
-  const list1: any = []; // 意图列表
 
   const onClose = () => {
     setVisible(false);
   };
 
-  useImperativeHandle(cref, () => ({
-    open: () => {
-      setVisible(true);
-    },
-    close: onClose,
-  }));
-
   const saveNode = () => {
     console.log(form.getFieldsValue());
   };
+
+  const bussinessList: any[] = [
+    {
+      name: '00',
+      label: '全部',
+    },
+    {
+      name: '01',
+      label: '火影忍者',
+    },
+    {
+      name: '02',
+      label: '死神',
+    },
+    {
+      name: '03',
+      label: '海贼王',
+    },
+    {
+      name: '04',
+      label: '死亡笔记',
+    },
+    {
+      name: '05',
+      label: '地狱骑士',
+    },
+  ];
 
   // 尾部 footer 代码
   const footer = (
@@ -42,67 +56,77 @@ const DrawerForm = (props: any) => {
     </div>
   );
 
-  // const addNew = () => {
-  //   const list = form.getFieldValue('list_1') || [];
-  //   // list.push({ response_speak: '' });
-  //   // form.setFieldsValue({ list_1: [...list] });
-  //   add({ response_speak: '' }, list.length);
-  // };
+  // 修改允许的业务节点
+  const onChangeRelationSelect = (val: any) => {
+    let _select: any = form.getFieldValue('allowFlows') || [];
+    let _banselect: any = form.getFieldValue('nonFlows') || [];
+    let lastKey = _select[_select.length - 1];
+    if (lastKey === '00') {
+      _select = ['00'];
+      _banselect = [];
+    } else {
+      _select = _select.filter((item: any) => item !== '00');
+      _banselect = _banselect.filter((item: any) => !val.includes(item));
+    }
+    form?.setFieldsValue({
+      allowFlows: _select,
+      nonFlows: _banselect,
+    });
+  };
+
+  // 修改禁止的业务节点
+  const onChangeRelationBanSelect = (val: any) => {
+    let _select: any = form.getFieldValue('allowFlows') || [];
+    let _banselect: any = form.getFieldValue('nonFlows') || [];
+    let lastKey = _select[_banselect.length - 1];
+    if (lastKey === '00') {
+      _banselect = ['00'];
+      _select = [];
+    } else {
+      _banselect = _banselect.filter((item: any) => item !== '00');
+      _select = _select.filter((item: any) => !val.includes(item));
+    }
+    form?.setFieldsValue({
+      allowFlows: _select,
+      nonFlows: _banselect,
+    });
+  };
 
   return (
     <Form form={form}>
-      <FormList name="list_1">
-        {(fields, { add }) => {
-          const addNew = () => {
-            const list = form.getFieldValue('list_1') || [];
-            // list.push({ response_speak: '' });
-            // form.setFieldsValue({ list_1: [...list] });
-            add({ response_speak: '' }, list.length);
-          };
+      <div className={styles['antd-form']}>
+        <FormItem name="allowFlows" label="允许跳转至业务流程" style={{ width: '400px' }}>
+          <Select
+            placeholder="请选择允许跳转至业务流程"
+            mode="multiple"
+            onChange={onChangeRelationSelect}
+          >
+            {bussinessList.map((item: any, index: number) => {
+              return (
+                <Option key={index} value={item.name} opt={item}>
+                  {item.label}
+                </Option>
+              );
+            })}
+          </Select>
+        </FormItem>
 
-          return (
-            <div style={{ paddingLeft: '20px' }}>
-              <div className={styles['zy-row']}>
-                <div className={styles['title_sec']} style={{ marginRight: '20px' }}>
-                  静默处理:
-                </div>
-                <Button type="link" icon={<PlusCircleOutlined />} onClick={addNew}></Button>
-              </div>
-
-              {fields.map((field: any, index: number) => (
-                <div key={index} className={styles['list-box']} style={{ marginLeft: '30px' }}>
-                  <div className={styles['num']}>{index + 1}.</div>
-                  <div>
-                    <Form.Item
-                      name={[field.name, 'response_speak']}
-                      fieldKey={[field.fieldKey, 'response_speak']}
-                      label="响应话术"
-                    >
-                      <Input placeholder="请输入响应话术" style={{ width: '400px' }} />
-                    </Form.Item>
-
-                    {/* <Form.Item
-                        name={[field.name, 'global_var']}
-                        fieldKey={[field.fieldKey, 'global_var']}
-                        label="选择变量"
-                      >
-                        <LabelSelect color="blue"></LabelSelect>
-                      </Form.Item>
-
-                      <Form.Item
-                        name={[field.name, 'label_var']}
-                        fieldKey={[field.fieldKey, 'label_var']}
-                        label="选择标签"
-                      >
-                        <LabelSelect color="orange"></LabelSelect>
-                      </Form.Item> */}
-                  </div>
-                </div>
-              ))}
-            </div>
-          );
-        }}
-      </FormList>
+        <FormItem name="nonFlows" label="禁止跳转至业务流程" style={{ width: '400px' }}>
+          <Select
+            placeholder="请选择禁止跳转至业务流程"
+            mode="multiple"
+            onChange={onChangeRelationBanSelect}
+          >
+            {bussinessList.map((item: any, index: number) => {
+              return (
+                <Option key={index} value={item.name} opt={item}>
+                  {item.label}
+                </Option>
+              );
+            })}
+          </Select>
+        </FormItem>
+      </div>
 
       {footer}
     </Form>
