85d822baa4e0c2f007185d44972f89c2e31f4d96
duan <freedom648754076@163.com>
2022-04-19 15:08:19 +0800

    fix(意图): 修复了一些bug

--- src/pages/gundam-pages/wish/index.tsx
+++ src/pages/gundam-pages/wish/index.tsx
@@ -48,7 +48,7 @@ const DetailPages: React.FC = (props: any) => {
         robotId: info.id,
         pageSize: pageData.pageSize,
         page: pageData.current,
-        headIntent: Number(pageData.headIntent),
+        headIntent: pageData?.headIntent ? Number(pageData.headIntent) : '',
       };
       const res: any = await getIntentTableList(params);
       return {
