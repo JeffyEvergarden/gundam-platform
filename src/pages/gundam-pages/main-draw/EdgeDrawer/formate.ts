135e5f2bb3261a2fa9fe8f4c36f185601c95897b
13113193306 <jiahao3306@163.com>
2022-04-29 09:05:03 +0800

    fix: 线加时间

--- src/pages/gundam-pages/main-draw/EdgeDrawer/formate.ts
+++ src/pages/gundam-pages/main-draw/EdgeDrawer/formate.ts
@@ -2,6 +2,7 @@
 import moment from 'moment';
 
 const reg = /\d{4}-\d{2}-\d{2}/;
+const reg2 = /\d{2}:\d{2}:\d{2}/;
 
 export const parserBody = (form: any) => {
   form = depyClone(form);
@@ -9,11 +10,19 @@ export const parserBody = (form: any) => {
 
   ruleList?.forEach((obj: any) => {
     obj?.rules?.forEach((item: any) => {
-      if (item?.ruleKeyType === 2 && item.valueType === 3) {
+      if ((item?.ruleKeyType === 2 || item?.ruleKeyType === 3) && item.valueType === 3) {
         // 日期格式
         if (item.ruleValue && reg.test(item.ruleValue)) {
           try {
             item.ruleValue = moment(item.ruleValue);
+          } catch (e) {
+            item.ruleValue = undefined;
+          }
+        } else if (item.ruleValue && reg2.test(item.ruleValue)) {
+          //时分秒
+          try {
+            let str = moment().format('YYYY-MM-DD');
+            item.ruleValue = moment(str + ' ' + item.ruleValue);
           } catch (e) {
             item.ruleValue = undefined;
           }
@@ -35,7 +44,11 @@ export const processRequest = (form: any) => {
   ruleList?.forEach((obj: any) => {
     obj?.rules?.forEach((item: any) => {
       if (item?.ruleValue instanceof moment) {
-        item.ruleValue = item.ruleValue.format('YYYY-MM-DD HH:mm:ss');
+        if (item?.ruleKeyType === 2) {
+          item.ruleValue = item.ruleValue.format('YYYY-MM-DD HH:mm:ss');
+        } else if (item?.ruleKeyType === 3) {
+          item.ruleValue = item.ruleValue.format('HH:mm:ss');
+        }
       }
     });
   });
