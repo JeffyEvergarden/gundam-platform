5e1b97c173481d88fbe5f4bdc91607ac4778fa18
13113193306 <jiahao3306@163.com>
2022-06-28 16:32:08 +0800

    fix: 回归分页问题

--- src/pages/gundam-pages/FAQ/components/question-list/index.tsx
+++ src/pages/gundam-pages/FAQ/components/question-list/index.tsx
@@ -294,14 +294,14 @@ const QuestionList: React.FC<any> = (props: any) => {
     setEdit([]);
 
     let res: any = await getFaqList(params);
-    if (params.page != 1) {
-      if (!res?.data?.list?.length) {
-        let num = current - 1;
-        setCurrent(num);
-        CurrentPage({ page: num, pageSize });
-        return;
-      }
-    }
+    // if (params.page != 1) {
+    //   if (!res?.data?.list?.length) {
+    //     let num = current - 1;
+    //     setCurrent(num);
+    //     CurrentPage({ page: num, pageSize });
+    //     return;
+    //   }
+    // }
 
     // console.log(res);
     getCreateUser(info.id, isRecycle);
