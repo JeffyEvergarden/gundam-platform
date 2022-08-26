5138e1b934e9d6bf10a5590c080d20045293c966
13113193306 <jiahao3306@163.com>
2022-08-26 17:36:57 +0800

    fix: 会话详情渠道列展示

--- src/pages/gundam-pages/knowledge-learn/component/sessionRecord/index.tsx
+++ src/pages/gundam-pages/knowledge-learn/component/sessionRecord/index.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
-import { Button, Modal, Space } from 'antd';
 import ChatRecordModal from '@/pages/gundam-pages/FAQ-module/components/chat-record-modal';
 import ProTable from '@ant-design/pro-table';
-import { channelMap } from '@/pages/gundam-pages/FAQ/const';
-import { useSessionList } from './model';
-import { history, useModel } from 'umi';
+import { Modal } from 'antd';
+import { useEffect, useRef } from 'react';
+import { useModel } from 'umi';
 import styles from './index.less';
+import { useSessionList } from './model';
 
 export default (props: any) => {
   const { visible, onCancel, modalData } = props;
@@ -13,6 +12,10 @@ export default (props: any) => {
 
   const { info } = useModel('gundam' as any, (model: any) => ({
     info: model.info,
+  }));
+
+  const { channelList } = useModel('drawer' as any, (model: any) => ({
+    channelList: model.channelList,
   }));
 
   const chatRecordModalRef = useRef<any>({});
@@ -63,7 +66,9 @@ export default (props: any) => {
       ellipsis: true,
       search: false,
       render: (t: any, r: any, i: any) => {
-        return <span>{channelMap[r.channelCode]}</span>;
+        return (
+          <span>{channelList?.find((item: any) => item.value == r.channelCode)?.label || ''}</span>
+        );
       },
     },
     {
