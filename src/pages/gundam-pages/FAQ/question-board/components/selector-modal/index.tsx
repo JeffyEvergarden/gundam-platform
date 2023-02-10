593b8e15693f107bf0a7fb6ac3421dfc043659f3
13113193306 <jiahao3306@163.com>
2023-02-10 16:03:22 +0800

    fix: 调自助服务

--- src/pages/gundam-pages/FAQ/question-board/components/selector-modal/index.tsx
+++ src/pages/gundam-pages/FAQ/question-board/components/selector-modal/index.tsx
@@ -419,39 +419,39 @@ const SelectorModal: React.FC<any> = (props: any) => {
           </div>
         </TabPane>
 
-        <Condition v-if={false}>
-          <TabPane tab="自助服务" key="3">
-            <div className={style['page_content']}>
-              <div className={style['zy-row_end']}>
-                <Search
-                  placeholder="请输入"
-                  value={searchText3}
-                  onSearch={onSearch3}
-                  onPressEnter={onSearch3}
-                  onChange={onSearchChange3}
-                  allowClear
-                  style={{ width: 300 }}
-                />
-              </div>
-
-              <div className={style['table-box']}>
-                <Table
-                  rowSelection={{
-                    type: type === 'radio' ? 'radio' : 'checkbox',
-                    ...rowSelfSelection,
-                    selectedRowKeys: selectedSelfKeys,
-                  }}
-                  size="small"
-                  pagination={{ current: current3, onChange: onChange3, total: selfTotalSize }}
-                  dataSource={selfList}
-                  columns={columns3}
-                  rowKey="selfId"
-                  // loading={tableLoading}
-                />
-              </div>
+        {/* <Condition v-if={false}> */}
+        <TabPane tab="自助服务" key="3">
+          <div className={style['page_content']}>
+            <div className={style['zy-row_end']}>
+              <Search
+                placeholder="请输入"
+                value={searchText3}
+                onSearch={onSearch3}
+                onPressEnter={onSearch3}
+                onChange={onSearchChange3}
+                allowClear
+                style={{ width: 300 }}
+              />
             </div>
-          </TabPane>
-        </Condition>
+
+            <div className={style['table-box']}>
+              <Table
+                rowSelection={{
+                  type: type === 'radio' ? 'radio' : 'checkbox',
+                  ...rowSelfSelection,
+                  selectedRowKeys: selectedSelfKeys,
+                }}
+                size="small"
+                pagination={{ current: current3, onChange: onChange3, total: selfTotalSize }}
+                dataSource={selfList}
+                columns={columns3}
+                rowKey="selfId"
+                // loading={tableLoading}
+              />
+            </div>
+          </div>
+        </TabPane>
+        {/* </Condition> */}
       </Tabs>
     </Modal>
   );
