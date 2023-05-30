b1f5b96282830dd02b7a71e2c473d9fd3bef70f6
13113193306 <jiahao3306@163.com>
2023-05-30 10:52:22 +0800

    fix: test

--- src/pages/gundam-pages/FAQ/question-board/index.tsx
+++ src/pages/gundam-pages/FAQ/question-board/index.tsx
@@ -221,7 +221,7 @@ const EditBoard: React.FC<any> = (prop: any) => {
 
   useEffect(() => {
     console.log('value:', value);
-    onChange?.(`<div>${value}</div>`);
+    onChange?.(`<p>${value}</p>`);
   }, []);
 
   useEffect(() => {
